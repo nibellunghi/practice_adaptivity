7210c0ef09eb668f85966f7d604ea49a2de0b5ed
nibellunghi <155111825+nibellunghi@users.noreply.github.com>
2024-05-24 18:43:36 +0300

    new burger menu

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,11 @@
 function myFunction() {
-    let x = document.getElementById("menu");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
+  let x = document.getElementById("menu");
+  let y = document.getElementById("flip");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+    y.style.transform = "rotateY(0deg)";
+  } else {
+    x.style.display = "flex";
+    y.style.transform = "rotateY(-180deg)";
   }
+}
